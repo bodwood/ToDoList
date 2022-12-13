df6e87927c10bd0215539c683f1a66abfa411035
bodwood <bodiewood@hotmail.com>
2022-12-12 16:53:48 -0800

    refactored Item.cs to match homework'

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -9,7 +9,7 @@ namespace ToDoList.Tests
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item();
+      Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());  //typeof returns data type of class, .GetType returns data type of a specific object
     }
     [TestMethod]
@@ -19,7 +19,21 @@ namespace ToDoList.Tests
       Item newItem = new Item(description); //create new item object, passing in the description (Arrange)
 
       string result = newItem.Description;  //set string to result of getting the Description property of newItem (Act)
+
       Assert.AreEqual(description, result); //make sure description retrieved from the Item object matches the description string (Assert)
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "Walk the dog";  //(Arrange)
+      Item newItem = new Item(description); //create new item object, passing in the description (Arrange)
+
+      string updatedDescription = "Do the dishes"; //(Arrange)
+      newItem.Description = updatedDescription; //(Arrange)
+      string result = newItem.Description;  //set string to result of getting the Description property of newItem (Act)
+
+      Assert.AreEqual(updatedDescription, result); //make sure description retrieved from the Item object matches the description string (Assert)
     }
   }
 }
